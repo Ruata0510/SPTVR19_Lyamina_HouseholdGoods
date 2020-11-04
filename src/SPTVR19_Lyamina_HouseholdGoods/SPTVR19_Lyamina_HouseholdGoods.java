e143777e07593c957af29dea05e5d2efe35b9758
svetlana titova <svetlanka65@inbox.ru>
2020-11-04 14:08:04 +0200

    SPTVR19_Lyamina_HouseholdGoods

---
+++ src/SPTVR19_Lyamina_HouseholdGoods/SPTVR19_Lyamina_HouseholdGoods.java
@@ -0,0 +1,10 @@
+package sptvr19_lyamina_householdgoods;
+
+public class SPTVR19_Lyamina_HouseholdGoods {
+    
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+    
+}
---
+++ src/tools/savers/Saveable.java
@@ -0,0 +1,9 @@
+package tools.savers;
+
+
+import java.util.List;
+
+interface Saveable {
+    public void saveToFile(List arrayList, String fileName);
+    public List loadFromFile(String fileName);
+}
